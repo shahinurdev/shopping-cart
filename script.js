a0a2c93a3748b4be17ff4c5047341bd9e1740609
shahinurdev <mdshahinur6@gmail.com>
2021-01-28 13:22:44 +0600

    task2

--- script.js
+++ script.js
@@ -1,10 +1,59 @@
 // btnPlus function
-const btnPlus = document.getElementById("btnPlus");
-btnPlus.addEventListener("click",function(){
-    const inputValue = document.getElementById("inputValue").value++;
-    const inputNumber = parseFloat(inputValue);
-    const phoneAmount = document.getElementById("phoneAmount").innerText;
-    const phoneAmountfloat = parseFloat(phoneAmount);
-    const newAmount = phoneAmountfloat * inputNumber ;
-    document.getElementById("phoneAmount").innerText = newAmount;
-})
+// document.getElementById("btnPlus").addEventListener("click", function () {
+//     productChange(true);
+// });
+// document.getElementById("btnMinus").addEventListener("click", function () {
+//     productChange(false);
+// });
+// document.getElementById("btnPlus").addEventListener("click", function () {
+//     const phoneCount = document.getElementById("phoneCount");
+//     const phoneCountInt = parseInt(phoneCount.value);
+//     const phoneCountPlus = phoneCountInt + 1;
+//     phoneCount.value = phoneCountPlus;
+//     const phoneAmount = phoneCountPlus * 1219;
+//     document.getElementById("phoneAmount").innerText = phoneAmount;
+// })
+// //btnMinus
+// document.getElementById("btnMinus").addEventListener("click", function () {
+//     const phoneCount = document.getElementById("phoneCount");
+//     const phoneCountInt = parseInt(phoneCount.value);
+//     const phoneCountPlus = phoneCountInt - 1;
+//     phoneCount.value = phoneCountPlus;
+//     const phoneAmount = phoneCountPlus * 1219;
+//     document.getElementById("phoneAmount").innerText = phoneAmount;
+// })
+// function Maintain
+function productChange(increase) {
+    const phoneCount = document.getElementById("phoneCount");
+    const phoneCountInt = parseInt(phoneCount.value);
+    // const phoneCountPlus = phoneCountInt + 1;
+    let phoneCountPlus = phoneCountInt;
+    if (increase == true) {
+
+        phoneCountPlus = phoneCountInt + 1;
+    }
+    if (increase == false && phoneCountInt > 0) {
+
+        phoneCountPlus = phoneCountInt - 1;
+    };
+    phoneCount.value = phoneCountPlus;
+    const phoneAmount = phoneCountPlus * 1219;
+    document.getElementById("phoneAmount").innerText = phoneAmount;
+};
+function caseChange(increase) {
+    const phoneCount = document.getElementById("caseCount");
+    const phoneCountInt = parseInt(phoneCount.value);
+    // const phoneCountPlus = phoneCountInt + 1;
+    let phoneCountPlus = phoneCountInt;
+    if (increase == true) {
+
+        phoneCountPlus = phoneCountInt + 1;
+    }
+    if (increase == false && phoneCountInt > 0) {
+
+        phoneCountPlus = phoneCountInt - 1;
+    };
+    phoneCount.value = phoneCountPlus;
+    const phoneAmount = phoneCountPlus * 59;
+    document.getElementById("caseAmount").innerText = phoneAmount;
+};
